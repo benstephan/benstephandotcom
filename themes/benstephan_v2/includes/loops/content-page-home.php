a3de891e5c92140ad767447b9f8873317cf035e5
benstephan <ben@benstephan.com>
2020-02-07 10:33:20 -0500

    Featured work responsiveness fix

--- themes/benstephan_v2/includes/loops/content-page-home.php
+++ themes/benstephan_v2/includes/loops/content-page-home.php
@@ -23,17 +23,17 @@ $thumb_id = get_post_thumbnail_id();
 $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'medium-size', true);
 $thumb_url = $thumb_url_array[0];
 ?>
+
 <div class="col-md">
-<div class="featured-work__wrap">
-<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-<img src="<?php echo get_relative_thumb( 'large' ); ?>" alt="<?php the_title(); ?>" class="img-responsive lazy" data-src="<?php echo get_relative_thumb( 'large' ); ?>" />
-<div class="featured-work__description">
-<h4><?php the_title(); ?></h4>
-<button class="btn">Read More</button>
+<figure>
+	<img src="<?php echo get_relative_thumb( 'large' ); ?>" alt="<?php the_title(); ?>" class="img-responsive lazy" data-src="<?php echo get_relative_thumb( 'large' ); ?>"/>
+	<figcaption>
+		<h2><?php the_title(); ?></h2>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">View more</a>
+	</figcaption>			
+</figure>
 </div>
-</a>
-</div>
-</div>
+
 <?php
 	}
 } else {
